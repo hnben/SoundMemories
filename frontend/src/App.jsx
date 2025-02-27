852b7319c07c357f129aedddbd71bab17d619729
William Castillo <william.castillo.jr@outlook.com>
2025-02-26 23:40:10 -0800

    Imported RequestAudioForm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css';
 import MoodButtons from './MoodButtons.jsx';
 import Recordings from './Recordings.jsx';
+import Modal from './RequestAudioForm.jsx';
+import { useState } from "react";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   //returns the frontend
   return (
@@ -10,6 +13,10 @@ function App() {
       <h1>Sound Memories Frontend Test</h1>
       <MoodButtons></MoodButtons>
       <Recordings></Recordings>
+      <button onClick={() => setIsModalOpen(true)}>Open Modal</button>
+
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+      </Modal>
     </>
   );
 }
