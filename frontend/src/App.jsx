021876d61720272427b5685f6a702d047b309ff3
William Castillo <william.castillo.jr@outlook.com>
2025-03-13 04:02:13 -0700

    Created route to record an audio message and added button to open up request audio form modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,19 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import "./App.css";
 import Recordings from "./Recordings.jsx";
 import ManageAudio from "./ManageAudio.jsx";
-import Modal from "./RequestAudioForm.jsx";
+import RecordAudioRequest from "./RecordAudioRequest.jsx";
+import RequestAudioForm from "./RequestAudioForm.jsx";
+
 import { useState } from "react";
 import Sidebar from "./Sidebar.jsx";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  {/* Button to open the modal */}
+  <button onClick={() => setIsModalOpen(true)}>
+    Request Audio Form
+  </button>
 
   return (
     <Router>
@@ -15,16 +22,16 @@ function App() {
         {/* Main content */}
         <Sidebar />
         <div className="main-content">
-          <h1>Sound Memories Frontend Test</h1>
+          <h1>Sound Memories</h1>
           <button onClick={() => setIsModalOpen(true)}>Open Modal</button>
         
         {/* Routes for the different*/}
           <Routes>
             <Route path="/" element={<Recordings />} />
             <Route path="/manage-audio" element={<ManageAudio />} />
+            <Route path="/contribute/3cq2i8eojyd1wb9jy03yem9mxkhajmga" element={<RecordAudioRequest />} />
           </Routes>
-
-          <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+          <RequestAudioForm isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
         </div>
       </div>
     </Router>
