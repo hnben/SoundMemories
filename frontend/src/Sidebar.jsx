c26effcdde54e46578b4a594d2052cbf19c47f3b
William Castillo <william.castillo.jr@outlook.com>
2025-04-02 18:34:07 -0700

    Moved RequestAudioForm button to sidebar

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+// import { useState } from "react";
 import "./Sidebar.css"; // Create a separate CSS file for styling
 
-const Sidebar = () => {
+const Sidebar = ({ setIsModalOpen }) => {
+
   return (
     <nav className="sidebar">
       <h2>Navigation</h2>
@@ -12,9 +15,17 @@ const Sidebar = () => {
         <li>
           <Link to="/manage-audio">Manage Audio</Link>
         </li>
+        <li>
+          <button id="request-audio-form"onClick={() => { setIsModalOpen(true)} }>Request Audio Form</button>
+        </li>
       </ul>
     </nav>
   );
+};
+
+// Define PropTypes for the Sidebar component
+Sidebar.propTypes = {
+  setIsModalOpen: PropTypes.func.isRequired,
 };
 
 export default Sidebar;
